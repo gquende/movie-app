fb1cbedd37b1b9040864d484c97c3f107312ca61
Geraldo Quende <adolfoquende@hotmail.com>
2022-03-11 12:35:11 +0100

    Main Page UI Part 2

--- movierestaapi/lib/pages/main_page.dart
+++ movierestaapi/lib/pages/main_page.dart
@@ -7,6 +7,8 @@ class MainPage extends ConsumerWidget {
   double _deviceHeight;
   double _deviceWidth;
 
+  TextEditingController _searchTextEditingController;
+
   @override
   Widget build(BuildContext context, ScopedReader watch) {
     _deviceHeight = MediaQuery.of(context).size.height;
@@ -24,13 +26,13 @@ class MainPage extends ConsumerWidget {
         height: _deviceHeight,
         child: Stack(
           alignment: Alignment.center,
-          children: [_background()],
+          children: [_backgroundWidget()],
         ),
       ),
     );
   }
 
-  Widget _background() {
+  Widget _backgroundWidget() {
     return Container(
       width: _deviceWidth,
       height: _deviceHeight,
@@ -47,6 +49,57 @@ class MainPage extends ConsumerWidget {
         child: Container(
           decoration: BoxDecoration(color: Colors.black.withOpacity(0.2)),
         ),
+      ),
+    );
+  }
+
+  Widget _foregroundWidget() {
+    return Container(
+      padding: EdgeInsets.fromLTRB(0, _deviceHeight * 0.02, 0, 0),
+      width: _deviceWidth * 0.88,
+      child: Column(
+        mainAxisSize: MainAxisSize.max,
+        mainAxisAlignment: MainAxisAlignment.end,
+        crossAxisAlignment: CrossAxisAlignment.center,
+        children: [_topBarWidget()],
+      ),
+    );
+  }
+
+  Widget _topBarWidget() {
+    return Container(
+      height: _deviceHeight * 0.08,
+      decoration: BoxDecoration(
+          color: Colors.black54, borderRadius: BorderRadius.circular(20.0)),
+      child: Row(
+        mainAxisSize: MainAxisSize.max,
+        mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+        crossAxisAlignment: CrossAxisAlignment.center,
+        children: [_searchFieldWidget()],
+      ),
+    );
+  }
+
+  Widget _searchFieldWidget() {
+    final _border = InputBorder.none;
+    return Container(
+      width: _deviceWidth * 0.50,
+      height: _deviceHeight * 0.05,
+      child: TextField(
+        controller: _searchTextEditingController,
+        onSubmitted: (_input) {},
+        style: TextStyle(color: Colors.white),
+        decoration: InputDecoration(
+            focusedBorder: _border,
+            border: _border,
+            prefixIcon: Icon(
+              Icons.search,
+              color: Colors.white24,
+            ),
+            filled: false,
+            fillColor: Colors.white24,
+            hintStyle: TextStyle(color: Colors.white54),
+            hintText: 'Search...'),
       ),
     );
   }
